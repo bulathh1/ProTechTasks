4c11b59b291a459145d1dc94f5e72e61ce45e5b4
bulathh1 <badgiev.bulat90@mail.ru>
2024-05-18 18:07:39 +0300

    Добавьте файлы проекта.

---
+++ ProTechTasks/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace ProTechTasks
+{
+    class StringW
+    {
+        private static string Reverse(string s)
+        {
+            char[] arr = s.ToCharArray();
+            int b = 5;
+            Array.Reverse(arr);
+            return new string(arr);
+        }
+        public static string StringEdit(string input)
+        {
+            if (input.Length % 2 == 0)
+            {
+                int mid = input.Length / 2;
+                string firstHalf = Reverse(input.Substring(0, mid));
+                string secondHalf = Reverse(input.Substring(mid));
+                return firstHalf + secondHalf;
+            }
+            else
+            {
+                string reversed = Reverse(input);
+                return reversed + input;
+            }
+        }
+    }
+    class Taks1
+    {
+        static void Main()
+        {
+            Console.WriteLine("Введите строку:");
+            string userInput = Console.ReadLine();
+            string result = StringW.StringEdit(userInput);
+            Console.WriteLine("Обработанная строка: " + result);
+        }
+    }
+}
