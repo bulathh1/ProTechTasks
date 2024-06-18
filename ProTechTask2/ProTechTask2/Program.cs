4cc03b40986765aa3d9e697a07802d00367dd543
bulathh1 <146529181+bulathh1@users.noreply.github.com>
2024-06-18 15:18:59 +0300

    Add files via upload

---
+++ ProTechTask2/ProTechTask2/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace ProTechTasks
+{
+    public static class ProTechTask2
+    {
+        static void Main(string[] args)
+        {
+            string c = Console.ReadLine();
+            var invalidChars = c.Where(c => !char.IsLower(c));
+            bool isValid = true;
+            foreach (char ch in c)
+            {
+                if (ch < 'a' || ch > 'z')
+                {
+                    Console.Write(ch);
+                    isValid = false;
+                }
+            }
+            if (isValid)
+            {
+                if (c.Length % 2 == 0)
+                {
+                    Console.WriteLine($"{reverse(c[..(c.Length / 2)]) + reverse(c.Substring(c.Length / 2))}");
+                }
+                else
+                {
+                    Console.WriteLine($"{reverse(c) + c}");
+                }
+            }
+
+            static string reverse(string message)
+            {
+                string hc = "";
+                foreach (var ch in message)
+                {
+                    hc = ch + hc;
+                }
+                return hc;
+            }
+        }
+    }
+}
